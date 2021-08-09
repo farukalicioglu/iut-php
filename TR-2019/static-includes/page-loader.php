3a5b744ae9a905c8b7e81c93b1b138af86f03264
Faruk Alıcıoğlu <farukalicioglu@gmail.com>
2021-08-09 12:55:48 +0300

    Delete TR-2019 directory

--- TR-2019/static-includes/page-loader.php
+++
@@ -1,7 +0,0 @@
-<div id="page-loader">
-	<div class="page-loader-inner">
-     	<span class="loader-figure"></span>
-   		<h6 class="loader-label">Yükleniyor</h6>
-        <img class="loader-logo" src="../files/iu_logo-240.png" srcset="../files/iu_logo-240.png 1x, ../files/iu_logo-240.png 2x" alt="Loader Logo">
-	</div>
-</div>
